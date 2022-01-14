3a4c3d827ed4ea521659ddbdf8bdf5d00d15c305
clambin <christophe.lambin@gmail.com>
2022-01-14 18:59:37 +0100

    test: fix crash when running "go test ./... -race" on macOS

--- server/server_test.go
+++ server/server_test.go
@@ -29,11 +29,11 @@ func TestRun(t *testing.T) {
 	}()
 
 	assert.Eventually(t, func() bool {
-		resp, err := http.Get("http://localhost:8080/metrics")
+		resp, err := http.Get("http://127.0.0.1:8080/metrics")
 		return err == nil && resp.StatusCode == http.StatusOK
 	}, 500*time.Millisecond, 10*time.Millisecond)
 
-	req, _ := http.NewRequest(http.MethodGet, "http://localhost:8080/api/v3/series", nil)
+	req, _ := http.NewRequest(http.MethodGet, "http://127.0.0.1:8080/api/v3/series", nil)
 	req.Header["X-Api-Key"] = []string{"12345"}
 	resp, err := http.DefaultClient.Do(req)
 	require.NoError(t, err)
@@ -45,7 +45,7 @@ func TestRun(t *testing.T) {
 	cancel()
 
 	assert.Never(t, func() bool {
-		resp, err = http.Get("http://localhost:8080/metrics")
+		resp, err = http.Get("http://127.0.0.1:8080/metrics")
 		return err == nil && resp.StatusCode == http.StatusOK
 	}, 100*time.Millisecond, 10*time.Millisecond)
 }
