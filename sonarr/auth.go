fd222c75b4e6ae8781a09f29b520c5369dc07828
clambin <christophe.lambin@gmail.com>
2022-12-05 09:14:06 +0100

    refactor: minor code change for clarity

--- sonarr/auth.go
+++ sonarr/auth.go
@@ -7,20 +7,20 @@ import (
 // AuthMiddleware checks that the request contains a valid API key
 func (handler *Handler) AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		var authenticated bool
-		for _, key := range req.Header["X-Api-Key"] {
-			authenticated = key == handler.APIKey
-			if authenticated {
-				break
-			}
-		}
-
-		if !authenticated {
+		if !authenticated(req, handler.APIKey) {
 			w.WriteHeader(http.StatusForbidden)
 			_, _ = w.Write([]byte("missing/invalid API key"))
 			return
 		}
-
 		next.ServeHTTP(w, req)
 	})
+}
+
+func authenticated(req *http.Request, apiKey string) bool {
+	for _, key := range req.Header["X-Api-Key"] {
+		if key == apiKey {
+			return true
+		}
+	}
+	return false
 }
