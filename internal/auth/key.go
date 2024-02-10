88a137fe56db6ff839c784178c81b59c040022af
Christophe Lambin <christophe.lambin@gmail.com>
2024-02-10 16:25:08 +0100

    build: Go 1.22

--- internal/auth/key.go
+++ internal/auth/key.go
@@ -7,9 +7,9 @@ import (
 
 // GenerateKey generates a Sonarr API Key
 func GenerateKey() (string, error) {
-	var key string
 	b := make([]byte, 16) // 16 bytes = 32 nibbles = 128 bits
 	_, err := rand.Read(b)
+	var key string
 	if err == nil {
 		key = hex.EncodeToString(b)
 	}
