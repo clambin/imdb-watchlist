0f3d8da1c62e35dc803a59e908e4ee3ab97947e0
Christophe Lambin <59534649+clambin@users.noreply.github.com>
2024-02-10 16:31:26 +0100

    build: move to Go 1.22 (#93)
    - change pipeline to use Go 1.22
    - make use of new Mux
    - use range-over-int where possible

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -7,18 +7,13 @@ const APIKeyHeader = "X-Api-Key"
 func Authenticate(apiKey string) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			var authenticated bool
 			for _, key := range req.Header.Values(APIKeyHeader) {
 				if key == apiKey {
-					authenticated = true
-					break
+					next.ServeHTTP(w, req)
+					return
 				}
 			}
-			if !authenticated {
-				http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
-				return
-			}
-			next.ServeHTTP(w, req)
+			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
 		})
 	}
 }
