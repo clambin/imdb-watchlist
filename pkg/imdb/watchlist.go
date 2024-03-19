64b856f8a03e8e257e5d9e14a194f91652fabc1e
Christophe Lambin <christophe.lambin@gmail.com>
2024-03-19 20:46:30 +0100

    refactor: simplify imdb.Watchlist.Filter(): not worth setting up a lookup map

--- pkg/imdb/watchlist.go
+++ pkg/imdb/watchlist.go
@@ -19,15 +19,13 @@ const (
 type Watchlist []Entry
 
 func (w Watchlist) Filter(mediaType ...EntryType) Watchlist {
-	mediaTypes := make(map[EntryType]struct{})
-	for i := range mediaType {
-		mediaTypes[mediaType[i]] = struct{}{}
-	}
-
 	filtered := make(Watchlist, 0, len(w))
 	for _, entry := range w {
-		if _, ok := mediaTypes[entry.Type]; ok {
-			filtered = append(filtered, entry)
+		for _, entryType := range mediaType {
+			if entry.Type == entryType {
+				filtered = append(filtered, entry)
+				break
+			}
 		}
 	}
 	return filtered
